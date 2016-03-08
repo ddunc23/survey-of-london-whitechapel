cfe9e46457c6146da701c4ec563bd20c27206c78
Duncan Hay <duncanhay@casa68.casa.ucl.ac.uk>
2016-03-08 11:06:19 +0000

    modified secret_settings_template.py

--- whitechapel/secret_settings_template.py
+++ whitechapel/secret_settings_template.py
@@ -1,2 +1,18 @@
 # Copy your secret key here and rename the file 'secret_settings.py'
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
