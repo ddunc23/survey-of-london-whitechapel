1e26ab3d58df1eb4f97b6686b993ca207a43c362
Duncan Hay <duncanhay@casa68.casa.ucl.ac.uk>
2016-03-08 09:31:52 +0000

    Removed /media/ from tracking, moved database settings to secret_settings.py

--- map_test/settings.py
+++ map_test/settings.py
@@ -82,6 +82,8 @@ WSGI_APPLICATION = 'map_test.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# Database connection settings are stored untracked in secret_settings.py
+
 # DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
@@ -89,17 +91,7 @@ WSGI_APPLICATION = 'map_test.wsgi.application'
 #    }
 #}
 
-DATABASES = {
-    'default': {
-        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'map_geodb',
-        'USER': 'geodjango',
-        'PASSWORD': 'whitechapel',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+
 
 # Serializers
 
