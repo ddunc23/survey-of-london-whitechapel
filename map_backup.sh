9adbe54ffd6e1f499b9ebb178c8789b9d93d893c
Duncan <hay.duncan@gmail.com>
2016-06-09 10:44:43 +0100

    accidently added the backup files to tracking. Removed them

--- map_backup.sh
+++ map_backup.sh
@@ -3,10 +3,10 @@
 # Script that creates backups of the SoL Whitechapel Database and Media Library. Probably a good idea to put this in your crontab in production. Fill in your own settings!
 
 CURRENT_DATE=$(date +'%Y-%m-%d')
-DB_BACKUP_DIR=/home/duncan/whitechapel_backups/databases
-MEDIA_DIR=/home/duncan/code/staging/survey-of-london-whitechapel/media/
-MEDIA_BACKUP_DIR=/home/duncan/whitechapel_backups/media
-DATABASE=whitechapel_map_staging
+DB_BACKUP_DIR=backups/databases
+MEDIA_DIR=media
+MEDIA_BACKUP_DIR=backups/media
+DATABASE=
 
 DB_FILENAME=${DB_BACKUP_DIR}/${DATABASE}_${CURRENT_DATE}.sql
 MEDIA_FILENAME=${MEDIA_BACKUP_DIR}/media_${CURRENT_DATE}.tar.gz
