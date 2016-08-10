f75070b1d0c22e412539a2e12e7335a594093999
Duncan <hay.duncan@gmail.com>
2016-08-10 15:25:44 +0100

    attempting to redirect social signups to the right page

--- whitechapel_users/adapter.py
+++ whitechapel_users/adapter.py
@@ -3,5 +3,6 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
-    def get_login_redirect_url(self, request):
+    def save_user(self, request, sociallogin, form=None):
+        super(DefaultSocialAccountAdapter, self).save_user(request, sociallogin, form=form)
         return HttpResponseRedirect(reverse('user_profile')
