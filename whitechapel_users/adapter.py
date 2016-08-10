691158f18cee5f8210d0ba38cd59fb8f49c6fd71
Duncan <hay.duncan@gmail.com>
2016-08-10 15:54:05 +0100

    still trying to fix this

--- whitechapel_users/adapter.py
+++ whitechapel_users/adapter.py
@@ -1,11 +1,12 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
+from datetime import datetime
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
     def get_login_redirect_url(self, request):
         # do your logic here for different social accounts
-        if hasattr(request.user, 'date_joined') == False:
+        if request.user.date_joined == datetime.now():
         	return HttpResponseRedirect(reverse('user_profile'))
         else:
         	return HttpResponseRedirect(reverse('map_home'))
