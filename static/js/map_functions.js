fc4a313df7cd1d10e99b9b96a2333fd5e5f6e5d7
Duncan <hay.duncan@gmail.com>
2016-09-07 12:07:18 +0100

    big explore button of excitement

--- static/js/map_functions.js
+++ static/js/map_functions.js
@@ -251,6 +251,8 @@ function loadFeatures(jsonUrl, mapType, allFeatures) {
 
 				rocquelayer = L.tileLayer('https://{s}.surveyoflondon.org/tileserver.php?/index.json?/rocque/{z}/{x}/{y}.png', {maxZoom: 20});
 				
+				/*rocquelayer = L.tileLayer('http://tile.casa.ucl.ac.uk/duncan/Whitechapel/OS_1873/{z}/{x}/{y}.png', {maxZoom: 20});*/
+
 				buildings = L.geoJson(geojson, {
 					onEachFeature: onEachFeature,
 				});
