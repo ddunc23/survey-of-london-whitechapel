e3f78b7283f363291c04734675390b9db129ee89
Duncan <hay.duncan@gmail.com>
2016-09-13 10:11:34 +0100

    getting to the bottom of this login email issue

--- whitechapel_users/urls.py
+++ whitechapel_users/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import include, url
 import views
 
 urlpatterns = [
-	url(r'$^', views.home, name='home'),
 	url(r'^check_first_login/$', views.check_first_login, name='check_first_login')
 ]
--- whitechapel_users/views.py
+++ whitechapel_users/views.py
@@ -12,9 +12,6 @@ from django.dispatch import receiver
 from django.core.mail import mail_managers, send_mail
 from django.core.mail.message import EmailMessage
 
-def home(request, user):
-	pass
-
 @login_required
 def user_profile(request):
 	"""View to allow users to edit their profile"""
@@ -48,13 +45,13 @@ def check_first_login(request):
 		return HttpResponseRedirect(reverse('map_home'))
 
 
-def send_welcome_email(user):
-	"""Send a welcome email to a new user"""
-	subject = 'Welcome to the Survey of London Whitechapel Project'
-	message = 'Thank you for joining us on the Whitechapel project. You have probably already explored the front page with its link to the interactive map, to blogs, and featured buildings, photos, interviews and film, and now you can add your own contributions. Just go to the map, click on the building you\'d like to tell us more about, and then the "Add Content" button. If you\'re not sure where the building is that you\'re looking for, there is a search field to help you find it. If you have any problems, just send us an email at solwhitechapel.bartlett@ucl.ac.uk. There is further information about how to add material at https://surveyoflondon.org/page/using-website/.\nWith best wishes,\nAileen Reid and Sarah Milne\nSurvey of London Whitechapel Project historians'
-	email = EmailMessage(subject, message, 'admin@surveyoflondon.org', [user.email], ['solwhitechapel.bartlett@ucl.ac.uk'])
-	email.send()
+#def send_welcome_email(user):
+#	"""Send a welcome email to a new user"""
+#	subject = 'Welcome to the Survey of London Whitechapel Project'
+#	message = 'Thank you for joining us on the Whitechapel project. You have probably already explored the front page with its link to the interactive map, to blogs, and featured buildings, photos, interviews and film, and now you can add your own contributions. Just go to the map, click on the building you\'d like to tell us more about, and then the "Add Content" button. If you\'re not sure where the building is that you\'re looking for, there is a search field to help you find it. If you have any problems, just send us an email at solwhitechapel.bartlett@ucl.ac.uk. There is further information about how to add material at https://surveyoflondon.org/page/using-website/.\nWith best wishes,\nAileen Reid and Sarah Milne\nSurvey of London Whitechapel Project historians'
+#	email = EmailMessage(subject, message, 'admin@surveyoflondon.org', [user.email], ['solwhitechapel.bartlett@ucl.ac.uk'])
+#	email.send()
 
-@receiver(user_signed_up)
-def send_signup_email(request, user, **kwargs):
-	send_welcome_email(user)
+#@receiver(user_signed_up)
+#def send_signup_email(request, user, **kwargs):
+#	send_welcome_email(user)
