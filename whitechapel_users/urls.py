33e10928359caa632b0848fcf0f4978643613abf
Duncan Hay <hay.duncan@gmail.com>
2018-04-16 14:49:23 +0100

    getting tired now

--- whitechapel_users/urls.py
+++ whitechapel_users/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import include, url
 import views
 
 urlpatterns = [
-	url(r'^login-check/$', views.check_first_login, name='check_first_login')
+	url(r'^login-check/$', views.check_first_login, name='check_first_login'),
+	url(r'^gdpr_prompt/$', views.gdpr_prompt, name='gdpr_prompt')
 ]
